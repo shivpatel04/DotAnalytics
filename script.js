717ab528b2c3cd8301be6044020f6172060cd419
shivpatel04 <143351147+shivpatel04@users.noreply.github.com>
2024-10-21 13:26:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+formCloseBtn = document.querySelector(".form_close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"))
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"))
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+        getPwInput.type = "text";
+        icon.classList.replace("uil-eye-slash", "uil-eye");
+    }else{
+        getPwInput.type = "password";
+        icon.classList.replace("uil-eye","uil-eye-slash");
+    }
+  })
+})
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
+
+var nextBtn = document.querySelector('.next'),
+    prevBtn = document.querySelector('.prev'),
+    carousel = document.querySelector('.carousel'),
+    list = document.querySelector('.list'), 
+    item = document.querySelectorAll('.item')
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        list.appendChild(sliderItemsDom[0])
+        carousel.classList.add('next')
+    } else{
+        list.prepend(sliderItemsDom[sliderItemsDom.length - 1])
+        carousel.classList.add('prev')
+    }
+  
+}
+
